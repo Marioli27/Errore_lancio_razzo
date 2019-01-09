6a70bce638956802ffd84c73cc5aa7d18f6f4700
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:02:40 +0100

    Calcoliamo prima l'errore di tempo e poi l'errore di spazio

---
+++ Errore_lancio_razzo/Errore_lancio_razzo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errore_lancio_razzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Spazio = 0;
+            double metri_sec_scud = 1676;
+            double errore_tempo = 0;
+            double tronc = 0.000000095;
+
+            //calcolo errore di tempo
+            errore_tempo = tronc * 100 * 60 * 60 * 10;
+            Console.WriteLine($"Hanno sbagliato di {errore_tempo}s");
+
+            //calcolo errore di spazio
+            Spazio = metri_sec_scud * errore_tempo;
+            Console.WriteLine($"Hanno sbagliato di {Spazio}m");
+
+            Console.ReadLine();
+        }
+    }
+}
