98788e7ff49185358fc38b449d7c5f524630b507
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:09:06 +0100

    abbiamo aggiunto la velocità in input

--- Errore_lancio_razzo/Errore_lancio_razzo/Program.cs
+++ Errore_lancio_razzo/Errore_lancio_razzo/Program.cs
@@ -11,16 +11,20 @@ namespace Errore_lancio_razzo
         static void Main(string[] args)
         {
             double Spazio = 0;
-            double metri_sec_scud = 1676;
+            double metri_sec = 0;
             double errore_tempo = 0;
             double tronc = 0.000000095;
+
+            //inseriamo in input la velocità
+            Console.WriteLine("inserire la velocità");
+            metri_sec = Convert.ToDouble(Console.ReadLine());
 
             //calcolo errore di tempo
             errore_tempo = tronc * 100 * 60 * 60 * 10;
             Console.WriteLine($"Hanno sbagliato di {errore_tempo}s");
 
             //calcolo errore di spazio
-            Spazio = metri_sec_scud * errore_tempo;
+            Spazio = metri_sec * errore_tempo;
             Console.WriteLine($"Hanno sbagliato di {Spazio}m");
 
             Console.ReadLine();
